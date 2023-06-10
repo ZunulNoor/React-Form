1ee4582247aebb3d3a634617722cd411048ccf37
Zun ul Noor <125741624+ZunulNoor@users.noreply.github.com>
2023-06-10 18:46:24 +0500

    Update RegistrationForm.jsx

--- src/Components/RegistrationForm.jsx
+++ src/Components/RegistrationForm.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function RegistrationForm() {
-   let istrue = false;
+   let istrue = true;
 
     return (
     <>
